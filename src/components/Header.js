bc41f0d47581d3c3933a4ec04c2995f02a2e3d8b
Brandon Nguyen <nbrandon62@gmail.com>
2023-02-02 16:13:34 -0800

    added a header for the website title

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "../css/header.css";
+
+const Header = () => {
+  return (
+    <div className="container">
+      <div className="header">#cheflife at home </div>
+    </div>
+  );
+};
+
+export default Header;
