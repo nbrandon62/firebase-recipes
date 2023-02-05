272ed4426eeaed8f027cc316f972287f7b8810c2
Brandon Nguyen <nbrandon62@gmail.com>
2023-02-05 12:00:13 -0800

    styled the instruction for margin left

--- src/pages/CreateRecipePage.js
+++ src/pages/CreateRecipePage.js
@@ -20,7 +20,7 @@ const CreateRecipePage = ({ handleAddRecipe }) => {
       <Grid columns="equal">
         <Grid.Row>
           <Grid.Column>
-            <p>
+            <p className="form-p1">
               Make the recipe as specific as you want. Describe the cooking process how you make it and others can make their twists if they want.
 
             </p>
@@ -32,7 +32,7 @@ const CreateRecipePage = ({ handleAddRecipe }) => {
           <Grid.Column>
           </Grid.Column>
           <Grid.Column>
-            <p>
+            <p className="form-p2">
               For the ingredients, enter rough amounts by measurement, whether it's cups, tablespoons, grams, etc  as long as you separate them by
               comma. The card will reformat it to a bulleted list.
             </p>
@@ -44,18 +44,10 @@ const CreateRecipePage = ({ handleAddRecipe }) => {
           <Grid.Column>
           </Grid.Column>
           <Grid.Column>
-            <p>For the method, you can write out a paragraph describing what to do so long as someone with cooking experience can decipher what to do. The card can also reformat the method into a numbered list, you just have to hyphenate each step</p>
+            <p className="form-p3">For the method, you can write out a paragraph describing what to do so long as someone with cooking experience can decipher what to do. The card can also reformat the method into a numbered list, you just have to hyphenate each step</p>
           </Grid.Column>
         </Grid.Row>
       </Grid>
-
-      {/* <h3 className="form-instruction">
-            Make it as specific as you want so long as someone with cooking
-            experience can follow it
-          </h3>
-          <h3 className="form-instruction-2">
-            You can use grams in your recipe if you want
-          </h3> */}
 
       <AddEditRecipeForm handleAddRecipe={handleAddRecipe} />
     </div>
