1a28a134836aa803c774771c76a425e099112c3d
Brandon Nguyen <nbrandon62@gmail.com>
2023-02-06 21:53:27 -0800

    added text and image as props to feed imagandinfo

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,25 @@
 import React, { useEffect } from "react";
 import CategoryList from "../components/CategoryList";
+import ImageAndInfoReverse from "../components/ImageAndInforReverse";
 import ImageAndInfo from "../components/ImageAndInfo";
 import Jumbrotron from "../components/Jumbrotron";
 import LoginForm from "../components/LoginForm";
 
+import chef2 from '../images/chef2.avif';
 
 const imageAndInfoProps = {
   description:
-    "This website serves as a place to gain inspiration when creative exhaustion hits. The recipes are divided into three categories: protein, starch, and vegetables. The recipes are written by ex chefs, home cooks, and the like. There is no mega scrolling through a life story to get to the ingredients list, only the ingredients, and the method, just like in a restaurant. Unlike in a restaurant, the recipes are meant to serve as a guide for the dish. I've linked YouTube to the footer of this site because it serves as a great tool for recipe research. Hopefully this website will help you figure out what to cook this week, and the week after that.",
+    "The recipes are divided into three categories: protein, starch, and vegetables. The recipes are written by ex chefs, home cooks, and the like. There is no mega scrolling through a life story to get to the ingredients list. There's only the ingredients and the method, just like in a restaurant. Unlike in a restaurant, the recipes are meant to serve as a guide for the dish. Hopefully this website will help you figure out what to cook this week, and the week after that.",
 };
+
+const browseProps = {
+  description: "#cheflife at home is a source of inspiration. I've linked YouTube to the footer of this site because it serves as a great tool for recipe research. My typical order of operations when it comes to researching a new dish to cook at home is: look at [insert food delivery app] to see what I would buy, write down the dishes I would buy and will now cook, go to YouTube to find the yummiest looking recipe. So grab a recipe from this site, look up how to make it, if you don't like what you see, look it up on YouTube and do it your way. It's your world. We're just living in it. ",
+  image: chef2
+}
+
+const jumboProps = {
+  header: "#cheflife #livelaughlove"
+}
 
 const Home = ({ existingUser }) => {
   useEffect(() => {
@@ -19,8 +30,9 @@ const Home = ({ existingUser }) => {
     <div>
       <LoginForm existingUser={existingUser} />
       <ImageAndInfo  description={imageAndInfoProps.description} />
-      <Jumbrotron />
-      <CategoryList />
+      <Jumbrotron  header={jumboProps.header}/>
+      <ImageAndInfoReverse description={browseProps.description} image={browseProps.image} />
+      {/* <CategoryList /> */}
     </div>
   );
 };
