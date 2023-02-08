c20e29ec62c806f6d28a156e6066799aa2176043
Brandon Nguyen <nbrandon62@gmail.com>
2023-02-07 17:34:45 -0800

    moved the handleFetchRecipeById function here and called it with a useEffect. Added useParams for id

--- src/pages/SingleRecipePage.js
+++ src/pages/SingleRecipePage.js
@@ -1,9 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
 
-const SingleRecipePage = () => {
+const SingleRecipePage = ({ handleFetchRecipeById }) => {
+  const [selectedRecipe, setSelectedRecipe] = useState("");
+
+  const {id} = useParams();
+
+  const fetchSingleRecipe = async () => {
+    const response = await handleFetchRecipeById(`${id}`);
+    setSelectedRecipe(response);
+  }
+
+  useEffect(() => {
+    fetchSingleRecipe();
+  }, [])
+
+  // const formatDate = (selectedRecipe) => {
+  //   const day = publishDate.getUTCDate();
+  //   const month = publishDate.getUTCMonth() + 1;
+  //   const year = publishDate.getFullYear();
+  //   const dateString = `${month}/${day}/${year}`;
+
+  //   return dateString;
+  // };
+
+  console.log(selectedRecipe);
+
+
   return (
-    <div>SingleRecipePage</div>
-  )
-}
+    <div>
+      <Link to="/recipes">
+        <button>back to products</button>
+      </Link>
+      <div>{selectedRecipe.title}</div>
+      <div>{selectedRecipe.ingredients}</div>
+      <div>{selectedRecipe.method}</div>
+      <div>{selectedRecipe.category}</div>
+      {/* <div>{formatDate(selectedRecipe)}</div> */}
+      
+    </div>
+  );
+};
 
-export default SingleRecipePage
+export default SingleRecipePage;
