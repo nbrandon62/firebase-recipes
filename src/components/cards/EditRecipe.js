44767b2182cb31772607929146d56e0bb17ab1de
nbrandon62 <nbrandon62@gmail.com>
2024-01-04 22:16:28 -0800

    lifted state out of uncontrolled component

--- src/components/cards/EditRecipe.js
+++ src/components/cards/EditRecipe.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react'
-import { useParams } from 'react-router-dom'
 import { BsPencilSquare } from 'react-icons/bs'
 
 import './styles/editrecipe.css'
@@ -7,36 +6,19 @@ import Input from '../form/Input'
 import TextArea from '../form/Textarea'
 
 const EditRecipe = ({
-  title,
-  method,
-  ingredients,
-  date,
+  recipe,
   user,
-  handleEditSubmit,
+  handleUpdateRecipe,
   handleShowEditClick,
 }) => {
-  const [newTitle, setNewTitle] = useState(title)
-  const [newMethod, setNewMethod] = useState(method)
-  const [newIngredients, setNewIngredients] = useState(ingredients)
-
-  const { id } = useParams()
-
-  const onEditSubmit = (id) => {
-    const updatedRecipe = {
-      title: newTitle,
-      method: newMethod,
-      ingredients: newIngredients,
-    }
-    handleEditSubmit(id, updatedRecipe)
-  }
 
   return (
     <div className='single-recipe__container'>
       <div className='header__container'>
         <Input
           className='edit__header__title'
-          value={newTitle}
-          onChange={(e) => setNewTitle(e.target.value)}
+          value={recipe.title}
+          onChange={(e) => handleUpdateRecipe({...recipe, title: e.target.value})}
         />
         <div className='header__icons'>
           {user ? (
@@ -55,16 +37,16 @@ const EditRecipe = ({
           <h2>method:</h2>
           <TextArea
             className='edit-body__textarea'
-            value={newMethod}
-            onChange={(e) => setNewMethod(e.target.value)}
+            value={recipe.method}
+            onChange={(e) => handleUpdateRecipe({...recipe, method: e.target.value})}
           />
         </div>
         <div className='edit-body__ingredients__container'>
           <h2>ingredients:</h2>
           <TextArea
             className='edit-body__textarea'
-            value={newIngredients}
-            onChange={(e) => setNewIngredients(e.target.value)}
+            value={recipe.ingredients}
+            onChange={(e) => handleUpdateRecipe({...recipe, ingredients: e.target.value})}
           />
         </div>
       </div>
