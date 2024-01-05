78f30096c0999e1ed07ce60f2b568d5ea9441396
nbrandon62 <nbrandon62@gmail.com>
2024-01-04 18:48:25 -0800

    css overhaul. Removed usage of media queries, float, for flexbox

--- src/components/cards/EditRecipe.js
+++ src/components/cards/EditRecipe.js
@@ -1,8 +1,10 @@
-import React, { useState } from "react";
-import { Link, useParams } from "react-router-dom";
-import { BsPencilSquare } from "react-icons/bs";
+import React, { useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { BsPencilSquare } from 'react-icons/bs'
 
-import "./styles/singlerecipecard.css";
+import './styles/editrecipe.css'
+import Input from '../form/Input'
+import TextArea from '../form/Textarea'
 
 const EditRecipe = ({
   title,
@@ -13,73 +15,61 @@ const EditRecipe = ({
   handleEditSubmit,
   handleShowEditClick,
 }) => {
+  const [newTitle, setNewTitle] = useState(title)
+  const [newMethod, setNewMethod] = useState(method)
+  const [newIngredients, setNewIngredients] = useState(ingredients)
 
-  const [newTitle, setNewTitle] = useState(title);
-  const [newMethod, setNewMethod] = useState(method);
-  const [newIngredients, setNewIngredients] = useState(ingredients);
-
-  const { id } = useParams();
+  const { id } = useParams()
 
   const onEditSubmit = (id) => {
     const updatedRecipe = {
       title: newTitle,
       method: newMethod,
       ingredients: newIngredients,
-    };
-    handleEditSubmit(id, updatedRecipe);
-  };
+    }
+    handleEditSubmit(id, updatedRecipe)
+  }
 
   return (
-    <div>
-      <div className="single-recipe-wrapper">
-        <div className="single-recipe-container">
-          <div className="single-recipe-card">
-            <div className="single-recipe-header-container">
-              <input
-                className="edit-title"
-                value={newTitle}
-                onChange={e => setNewTitle(e.target.value)}
-              ></input>
-              {user ? (
-                <>
-                  <BsPencilSquare
-                    className="pencil-icon"
-                    onClick={(e) => handleShowEditClick()}
-                  />
-                </>
-              ) : null}
-            </div>
-
-            <div className="sr-col-wrapper">
-              <h5 className="single-recipe-date">{date}</h5>
-              <div className="sr-col-ingredients">
-                <h3 className="single-recipe-title">ingredients:</h3>
-                <textarea
-                  className="edit-ingredients"
-                  value={newIngredients}
-                  onChange={(e) => setNewIngredients(e.target.value)}
-                ></textarea>
-              </div>
-              <div className="sr-col-method">
-                <h3 className="single-recipe-title">method:</h3>
-                <textarea
-                  className="edit-method"
-                  value={newMethod}
-                  onChange={(e) => setNewMethod(e.target.value)}
-                ></textarea>
-
-              </div>
-            </div>
-          </div>
-          <div className="sr-button-container">
-            <button className="ui button" onClick={(e) => onEditSubmit(id)}>
-              Save
-            </button>
-          </div>
+    <div className='single-recipe__container'>
+      <div className='header__container'>
+        <Input
+          className='edit__header__title'
+          value={newTitle}
+          onChange={(e) => setNewTitle(e.target.value)}
+        />
+        <div className='header__icons'>
+          {user ? (
+            <>
+              <BsPencilSquare
+                className='pencil-icon'
+                onClick={(e) => handleShowEditClick()}
+              />
+            </>
+          ) : null}
+        </div>
+      </div>
+      
+      <div className='edit-body__container'>
+        <div className='edit-body__method__container'>
+          <h2>method:</h2>
+          <TextArea
+            className='edit-body__textarea'
+            value={newMethod}
+            onChange={(e) => setNewMethod(e.target.value)}
+          />
+        </div>
+        <div className='edit-body__ingredients__container'>
+          <h2>ingredients:</h2>
+          <TextArea
+            className='edit-body__textarea'
+            value={newIngredients}
+            onChange={(e) => setNewIngredients(e.target.value)}
+          />
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default EditRecipe;
+export default EditRecipe
